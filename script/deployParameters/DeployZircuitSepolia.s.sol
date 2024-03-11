b2266e01f1e36374a645d65dadba89b020901b98
leon-do <leondo123@gmail.com>
2024-03-11 18:39:52 -0400

    deploy zircuit sepolia

---
+++ script/deployParameters/DeployZircuitSepolia.s.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+import {DeployUniversalRouter} from '../DeployUniversalRouter.s.sol';
+import {RouterParameters} from 'contracts/base/RouterImmutables.sol';
+
+contract DeployZircuitSepolia is DeployUniversalRouter {
+    function setUp() public override {
+        params = RouterParameters({
+            permit2: 0x000000000022D473030F116dDEE9F6B43aC78BA3,
+            weth9: 0xB624103Eb622d655dd834B694Aa6EA0363f2A156,
+            seaportV1_5: UNSUPPORTED_PROTOCOL,
+            seaportV1_4: UNSUPPORTED_PROTOCOL,
+            openseaConduit: UNSUPPORTED_PROTOCOL,
+            nftxZap: UNSUPPORTED_PROTOCOL,
+            x2y2: UNSUPPORTED_PROTOCOL,
+            foundation: UNSUPPORTED_PROTOCOL,
+            sudoswap: UNSUPPORTED_PROTOCOL,
+            elementMarket: UNSUPPORTED_PROTOCOL,
+            nft20Zap: UNSUPPORTED_PROTOCOL,
+            cryptopunks: UNSUPPORTED_PROTOCOL,
+            looksRareV2: UNSUPPORTED_PROTOCOL,
+            routerRewardsDistributor: UNSUPPORTED_PROTOCOL,
+            looksRareRewardsDistributor: UNSUPPORTED_PROTOCOL,
+            looksRareToken: UNSUPPORTED_PROTOCOL,
+            v2Factory: UNSUPPORTED_PROTOCOL,
+            v3Factory: 0x7610a9F3b98705B578AE8142Fd107813ebA42139,
+            pairInitCodeHash: BYTES32_ZERO,
+            poolInitCodeHash: 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54
+        });
+
+        unsupported = 0x003D0d8a8F2a5Acc8e400287FfC519837ad7C7e5;
+    }
+}
